0f83589cfd6ed1bb42ff3b334185774aa7c410f0
Sky <alschuler.s@gmail.com>
2020-03-30 17:52:17 -0700

    Refactors auth using redux

--- twooch/client/src/components/GoogleAuth.jsx
+++ twooch/client/src/components/GoogleAuth.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class GoogleAuth extends Component {
-  state = { isSignedIn: null };
+import { signIn, signOut } from "../actions";
 
+class GoogleAuth extends Component {
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
       window.gapi.client
@@ -13,14 +14,18 @@ export default class GoogleAuth extends Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
   };
 
   onSignInClick = () => {
@@ -32,9 +37,9 @@ export default class GoogleAuth extends Component {
   };
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
@@ -55,3 +60,12 @@ export default class GoogleAuth extends Component {
     return <div>{this.renderAuthButton()}</div>;
   }
 }
+
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, {
+  signIn,
+  signOut
+})(GoogleAuth);
--- twooch/client/src/reducers/index.js
+++ twooch/client/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 
+import authReducer from "./authReducer";
+
 export default combineReducers({
-  replaceMe: () => "from dummy reducer"
+  auth: authReducer
 });
